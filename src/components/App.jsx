7380bcaead6bc8398ccaa0e221326e50b24179ac
AnwarHossainSR <mahedianwar@gmail.com>
2021-03-18 21:01:46 +0600

    To do List with material icon

---
+++ src/components/App.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import ToDoItem from "./ToDoItem";
+import AddIcon from '@material-ui/icons/Add';
+
+function App() {
+  const [task, setTask] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setTask(newValue);
+  }
+
+  function addTask() {
+    setItems(prevValues => {
+      return [...prevValues, task];
+    });
+    setTask("");
+  }
+
+  function deleteItem(id) {
+    setItems(prevValues => {
+      return prevValues.filter((item, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input name="taskInput" type="text" onChange={handleChange} value={task}
+        />
+        <button onClick={addTask}>
+          <span><AddIcon /></span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {items.map((item, index) => (
+            <ToDoItem key={index}  id={index} text={item} onChecked={deleteItem} />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
